c982733eaac1146786935490a6fcf142c8ba3640
felixokolo <pheelix.info@gmail.com>
2022-06-14 12:49:13 +0100

    addition

---
+++ monty.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - Main monty function
+ * @argc: number of arguments
+ * @argv: Array of arguments
+ * Return - 0
+ */
+
+int main(int argc, char **argv)
+{
+	
+}
---
+++ str_man.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "monty.h"
+
+
+/**
+ * get_instr - Gets instruction from file
+ * @f: String of file name to obtain instruction
+ * @line: Line to get instruction
+ * Return: instruction_t
+ */
+
+instruction_t get_instr(char *f, int line)
+{
+	
+}
